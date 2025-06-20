c7bd2d40304d34ee8e994adab3c700e919c819a2
Arthur <ishmukhamedovartur03@gmail.com>
2025-06-20 20:40:59 +0300

    ABoba2

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Drawer.css';
 
-function Drawer({ isOpen, onClose }) {
+function DrawerToggleWrapper() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDrawer = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={`drawer ${isOpen ? 'open' : ''}`}>
-      <button className="drawer-close" onClick={onClose}>×</button>
-      <ul className="drawer-menu">
-        <li>Настройки</li>
-        <li>О приложении</li>
-        <li>Поддержка</li>
-      </ul>
-    </div>
+    <>
+      <button className="drawer-toggle" onClick={toggleDrawer}>☰</button>
+      <div className={`drawer ${isOpen ? 'open' : ''}`}>
+        <button className="drawer-close" onClick={closeDrawer}>×</button>
+        <ul className="drawer-menu">
+          <li>Настройки</li>
+          <li>О приложении</li>
+          <li>Поддержка</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
-export default Drawer;
+export default DrawerToggleWrapper;
