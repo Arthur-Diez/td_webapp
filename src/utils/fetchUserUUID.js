2588f67ebc9efd3aaeaff57130c92d5968e98af8
Arthur <ishmukhamedovartur03@gmail.com>
2025-07-21 21:47:18 +0300

    dsdsds

--- src/utils/fetchUserUUID.js
+++ src/utils/fetchUserUUID.js
@@ -1,12 +1,12 @@
 // src/utils/fetchUserUUID.js
 export async function fetchUserUUID(tgId) {
   try {
-    const res  = await fetch(
-      `https://td-webapp.onrender.com/user_id?tg_id=${tgId}`
+    const res = await fetch(
+      `https://td-webapp.onrender.com/user_id?tg_id=${Number(tgId)}`
     );
     const data = await res.json();
     if (data.id) {
-      return data.id;                 // ← UUID найден
+      return data.id;
     }
     console.error("Ошибка получения UUID:", data.error);
     return null;
