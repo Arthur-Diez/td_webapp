e792b02c7f9b0bea215f41ffdcfe960718d5db11
Arthur-Diez <ishmukhamedovartur03@gmail.com>
2025-08-18 00:16:04 +0300

    New serv

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // src/utils/api.js
-// ⚠️ замени IP и порт на свои (если настроишь nginx — будет https и 443)
-export const API_BASE = 'http://147.45.167.44:8000';
+// Было: http://147.45.167.44:8000
+export const API_BASE = 'https://api.freakdev.site';
 export const api = (path) => `${API_BASE}${path}`;
